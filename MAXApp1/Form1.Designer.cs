68eb720a9dee6963c50310a69c65e38cd919c9f5
MAX <ot_525@hotmail.com>
2024-07-25 10:34:22 +0800

    加入專案檔案。

---
+++ MAXApp1/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace MAXApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btQuit = new Button();
+            btSave = new Button();
+            dfIDNumber = new TextBox();
+            dfBirthday = new TextBox();
+            dfChineseName = new TextBox();
+            dfEmployeCd = new TextBox();
+            lbIDNumber = new Label();
+            lbBirthday = new Label();
+            lbChineseName = new Label();
+            lbEmployeCd = new Label();
+            tabPage2 = new TabPage();
+            btQuit2 = new Button();
+            pbImport = new Button();
+            dataGridView1 = new DataGridView();
+            colEmployeCd = new DataGridViewTextBoxColumn();
+            colChineseName = new DataGridViewTextBoxColumn();
+            coBirthday = new DataGridViewTextBoxColumn();
+            colIDNumber = new DataGridViewTextBoxColumn();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Font = new Font("微軟正黑體", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(tableLayoutPanel1);
+            tabPage1.Controls.Add(btQuit);
+            tabPage1.Controls.Add(btSave);
+            tabPage1.Controls.Add(dfIDNumber);
+            tabPage1.Controls.Add(dfBirthday);
+            tabPage1.Controls.Add(dfChineseName);
+            tabPage1.Controls.Add(dfEmployeCd);
+            tabPage1.Controls.Add(lbIDNumber);
+            tabPage1.Controls.Add(lbBirthday);
+            tabPage1.Controls.Add(lbChineseName);
+            tabPage1.Controls.Add(lbEmployeCd);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 417);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "資料建檔";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoScroll = true;
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.BackgroundImageLayout = ImageLayout.Center;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Location = new Point(335, 50);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new Size(378, 115);
+            tableLayoutPanel1.TabIndex = 5;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // btQuit
+            // 
+            btQuit.Location = new Point(175, 240);
+            btQuit.Name = "btQuit";
+            btQuit.Size = new Size(120, 45);
+            btQuit.TabIndex = 9;
+            btQuit.Text = "離開";
+            btQuit.UseVisualStyleBackColor = true;
+            btQuit.Click += btQuit_Click;
+            // 
+            // btSave
+            // 
+            btSave.Location = new Point(29, 240);
+            btSave.Name = "btSave";
+            btSave.Size = new Size(120, 45);
+            btSave.TabIndex = 8;
+            btSave.Text = "存檔";
+            btSave.UseVisualStyleBackColor = true;
+            btSave.Click += btSave_Click;
+            // 
+            // dfIDNumber
+            // 
+            dfIDNumber.Location = new Point(175, 183);
+            dfIDNumber.Name = "dfIDNumber";
+            dfIDNumber.Size = new Size(100, 29);
+            dfIDNumber.TabIndex = 7;
+            // 
+            // dfBirthday
+            // 
+            dfBirthday.Location = new Point(175, 136);
+            dfBirthday.Name = "dfBirthday";
+            dfBirthday.Size = new Size(100, 29);
+            dfBirthday.TabIndex = 6;
+            // 
+            // dfChineseName
+            // 
+            dfChineseName.Location = new Point(175, 94);
+            dfChineseName.Name = "dfChineseName";
+            dfChineseName.Size = new Size(100, 29);
+            dfChineseName.TabIndex = 5;
+            // 
+            // dfEmployeCd
+            // 
+            dfEmployeCd.Location = new Point(175, 52);
+            dfEmployeCd.Name = "dfEmployeCd";
+            dfEmployeCd.Size = new Size(100, 29);
+            dfEmployeCd.TabIndex = 4;
+            // 
+            // lbIDNumber
+            // 
+            lbIDNumber.AutoSize = true;
+            lbIDNumber.Location = new Point(29, 183);
+            lbIDNumber.Name = "lbIDNumber";
+            lbIDNumber.Size = new Size(73, 20);
+            lbIDNumber.TabIndex = 3;
+            lbIDNumber.Text = "身分證號";
+            // 
+            // lbBirthday
+            // 
+            lbBirthday.AutoSize = true;
+            lbBirthday.Location = new Point(29, 136);
+            lbBirthday.Name = "lbBirthday";
+            lbBirthday.Size = new Size(41, 20);
+            lbBirthday.TabIndex = 2;
+            lbBirthday.Text = "生日";
+            // 
+            // lbChineseName
+            // 
+            lbChineseName.AutoSize = true;
+            lbChineseName.Location = new Point(29, 94);
+            lbChineseName.Name = "lbChineseName";
+            lbChineseName.Size = new Size(73, 20);
+            lbChineseName.TabIndex = 1;
+            lbChineseName.Text = "中文姓名";
+            // 
+            // lbEmployeCd
+            // 
+            lbEmployeCd.AutoSize = true;
+            lbEmployeCd.Location = new Point(29, 52);
+            lbEmployeCd.Name = "lbEmployeCd";
+            lbEmployeCd.Size = new Size(73, 20);
+            lbEmployeCd.TabIndex = 0;
+            lbEmployeCd.Text = "員工編號";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btQuit2);
+            tabPage2.Controls.Add(pbImport);
+            tabPage2.Controls.Add(dataGridView1);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 417);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "資料維護";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btQuit2
+            // 
+            btQuit2.Location = new Point(54, 161);
+            btQuit2.Name = "btQuit2";
+            btQuit2.Size = new Size(151, 44);
+            btQuit2.TabIndex = 10;
+            btQuit2.Text = "離開";
+            btQuit2.UseVisualStyleBackColor = true;
+            btQuit2.Click += btQuit2_Click;
+            // 
+            // pbImport
+            // 
+            pbImport.Location = new Point(54, 85);
+            pbImport.Name = "pbImport";
+            pbImport.Size = new Size(151, 47);
+            pbImport.TabIndex = 1;
+            pbImport.Text = "轉入";
+            pbImport.UseVisualStyleBackColor = true;
+            pbImport.Click += pbImport_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { colEmployeCd, colChineseName, coBirthday, colIDNumber });
+            dataGridView1.Location = new Point(278, 34);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(446, 300);
+            dataGridView1.TabIndex = 0;
+            // 
+            // colEmployeCd
+            // 
+            colEmployeCd.HeaderText = "員工編號";
+            colEmployeCd.Name = "colEmployeCd";
+            // 
+            // colChineseName
+            // 
+            colChineseName.HeaderText = "中文姓名";
+            colChineseName.Name = "colChineseName";
+            // 
+            // coBirthday
+            // 
+            coBirthday.HeaderText = "生日";
+            coBirthday.Name = "coBirthday";
+            // 
+            // colIDNumber
+            // 
+            colIDNumber.HeaderText = "身分證號";
+            colIDNumber.Name = "colIDNumber";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label lbIDNumber;
+        private Label lbBirthday;
+        private Label lbChineseName;
+        private Label lbEmployeCd;
+        private TextBox dfIDNumber;
+        private TextBox dfBirthday;
+        private TextBox dfChineseName;
+        private TextBox dfEmployeCd;
+        private Button btSave;
+        private Button btQuit;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn colEmployeCd;
+        private DataGridViewTextBoxColumn colChineseName;
+        private DataGridViewTextBoxColumn coBirthday;
+        private DataGridViewTextBoxColumn colIDNumber;
+        private Button pbImport;
+        private Button btQuit2;
+    }
+}
---
+++ MAXApp1/Form1.cs
@@ -0,0 +1,148 @@
+namespace MAXApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 設置表頭
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "員工編號", Font = new Font("Arial", 10, FontStyle.Bold) }, 0, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "中文姓名", Font = new Font("Arial", 10, FontStyle.Bold) }, 1, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "生日", Font = new Font("Arial", 10, FontStyle.Bold) }, 2, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "身分證號", Font = new Font("Arial", 10, FontStyle.Bold) }, 3, 0);
+
+            // 設置邊框樣式
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            // 根據需要設置 TableLayoutPanel 的其他屬性
+            tableLayoutPanel1.AutoSize = true;
+            //tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowOnly;
+        }
+
+
+        private void btQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+
+            // 獲取輸入資料
+            string employeCd = dfEmployeCd.Text;
+            string chineseName = dfChineseName.Text;
+            string birthday = dfBirthday.Text;
+            string idNumber = dfIDNumber.Text;
+
+            // 計算新的行索引
+            int newRowIndex = tableLayoutPanel1.RowCount;
+
+            // 增加新的行
+            //tableLayoutPanel1.RowCount += 1;
+            tableLayoutPanel1.RowCount = tableLayoutPanel1.RowCount + 1;
+            // 調整行高樣式確保每行高度一致
+            tableLayoutPanel1.RowStyles.Clear();
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / tableLayoutPanel1.RowCount));
+            }
+
+            // 添加資料到新的行
+            tableLayoutPanel1.Controls.Add(new Label() { Text = employeCd }, 0, newRowIndex);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = chineseName }, 1, newRowIndex);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = birthday }, 2, newRowIndex);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = idNumber }, 3, newRowIndex);
+
+            // 顯示成功訊息
+            MessageBox.Show("存檔成功！");
+
+            // 清空文本框
+            dfEmployeCd.Text = string.Empty;
+            dfChineseName.Text = string.Empty;
+            dfBirthday.Text = string.Empty;
+            dfIDNumber.Text = string.Empty;
+        }
+
+        private void pbImport_Click(object sender, EventArgs e)
+        {
+            // 暫停 dataGridView1 的佈局
+            dataGridView1.SuspendLayout();
+
+            try
+            {
+                // 1. 從 tableLayoutPanel1 中讀取資料
+                List<string[]> data = new List<string[]>();
+
+                for (int row = 1; row < tableLayoutPanel1.RowCount; row++)
+                {
+                    List<string> rowData = new List<string>();
+                    bool isEmptyRow = true;
+
+                    for (int col = 0; col < tableLayoutPanel1.ColumnCount; col++)
+                    {
+                        Control control = tableLayoutPanel1.GetControlFromPosition(col, row);
+                        if (control != null)
+                        {
+                            rowData.Add(control.Text);
+                            isEmptyRow = false;
+                        }
+                        else
+                        {
+                            rowData.Add(string.Empty); // 如果控件為空，添加空字串
+                        }
+                    }
+
+                    if (!isEmptyRow)
+                    {
+                        data.Add(rowData.ToArray());
+                    }
+                }
+
+                // 2. 根據第一個欄位排序
+                var sortedData = data.OrderBy(row => row[0]).ToArray();
+
+                // 3. 清空 dataGridView1 中的資料，但保留列設置
+                dataGridView1.Rows.Clear();
+
+                // 4. 添加排序後的資料到 dataGridView1
+                foreach (var row in sortedData)
+                {
+                    dataGridView1.Rows.Add(row);
+                }
+            }
+            finally
+            {
+                // 恢復 dataGridView1 的佈局
+                dataGridView1.ResumeLayout();
+            }
+        }
+
+        private void btQuit2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+}
+
+
